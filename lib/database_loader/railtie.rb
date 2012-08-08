769dcb5daa345da0a334ad94c288f70131e61c4a
Hugo Duksis <hugo.duksis@tieto.com>
2012-08-08 10:43:36 +0300

    Load custom config if it exists

--- lib/database_loader/railtie.rb
+++ lib/database_loader/railtie.rb
@@ -1,6 +1,8 @@
 module DatabaseLoader
   class Railtie < Rails::Railtie
     rake_tasks do
+      custom_config_file = "#{Rails.root}/config/initializers/database_loader.rb"
+      require custom_config_file if File.exists?(custom_config_file)
       DatabaseLoader.set_schemas
       require "database_loader/tasks"
     end
